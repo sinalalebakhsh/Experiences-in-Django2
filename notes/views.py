8d6fc965bc7fc267bbbd3030c73ba40b71af54c5
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-21 12:21:56 +0430

    Settings templates/home.html for show in browser

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Note
 
 def list_views(request):
-    return render(request, 'home.html')
+    notes = Note.objects.all()
+    context = {
+        'list_views': notes,
+    }
+    return render(request, 'home.html', context)
