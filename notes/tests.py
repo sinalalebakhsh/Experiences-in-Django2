4b98e565065310234d7633b670f342a7eb8a02ec
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-21 12:29:42 +0430

    Setting First Test in tests.py

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class NotesListViews(TestCase):
+    def test_note_list_views_in_browser(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
