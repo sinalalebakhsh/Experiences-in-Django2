361cca4fa8f9ff40d7b8bc61ce792c0e36007116
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2022-05-21 12:38:00 +0430

    Setting Second Test in tests.py

--- notes/tests.py
+++ notes/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
 class NotesListViews(TestCase):
     def test_note_list_views_in_browser(self):
         response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+    def test_name_urls_patterns(self):
+        response = self.client.get(reverse('list views'))
         self.assertEqual(response.status_code, 200)
